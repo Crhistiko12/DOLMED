b035558b5826d1642222532075e80c3226eb4a52
Crhistiko12 <almanzacoorp@gmail.com>
2025-11-24 09:18:50 -0400

    Se actualizo para render

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,6 @@ bundle install
 bundle exec rails css:build
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+# Prepare database (creates if doesn't exist)
+bundle exec rails db:prepare
 bundle exec rails db:migrate
